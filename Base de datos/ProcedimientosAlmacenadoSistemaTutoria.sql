4bf50ca1893116c4ae90c78f708d2c26d6655d87
Diana743238 <78519505+Diana743238@users.noreply.github.com>
2021-08-20 15:45:43 -0500

    Update ProcedimientosAlmacenadoSistemaTutoria.sql

--- Base de datos/ProcedimientosAlmacenadoSistemaTutoria.sql
+++ Base de datos/ProcedimientosAlmacenadoSistemaTutoria.sql
@@ -65,11 +65,42 @@ CREATE PROCEDURE spuListarAlumnosGrupoAula @CodAula varchar(10)
 AS
 begin
    SELECT *
-   FROM TGrupoEstudiante G inner join TAulaTutoria A
+   FROM TAlumno A inner join ( SELECT * from TGrupoEstudiante G inner join TAulaTutoria A  
    ON G.CodGrupoEstudiante=A.CodGrupoAlumno
-   WHERE A.CodAulaTutoria =@CodAula
+   WHERE A.CodAulaTutoria =@CodAula) H
+   on A.CodAlumno= H.CodAlumno
 END;
 go
 
---drop PROCEDURE spuConsultarAulaTutoriaDT
+--drop PROCEDURE spuListarAlumnosGrupoAula
 --EXEC spuListarAlumnosGrupoAula 'A0001'
+
+CREATE PROCEDURE spuGenerarListaAlumnos @Cantidad INT
+AS
+begin
+   SELECT TOP (@Cantidad) A.*
+   FROM   TAlumno A
+   WHERE NOT EXISTS( SELECT * FROM TGrupoEstudiante G
+                     WHERE A.CodAlumno=G.CodAlumno)
+   ORDER BY NEWID()
+END;
+go
+
+--drop PROCEDURE spuGenerarListaAlumnos
+--EXEC spuGenerarListaAlumnos 11
+ 
+CREATE PROCEDURE spuCalcularAlumnoGrupo @NroGrupos INT OUTPUT
+AS
+BEGIN
+  DECLARE @CantidadAlumnos INT;
+  DECLARE @CantidadTutores INT;
+  SELECT  @CantidadAlumnos = COUNT(*) FROM TAlumno A;
+  SELECT @CantidadTutores = COUNT(*) FROM TAulaTutoria;
+  SELECT @NroGrupos =  @CantidadAlumnos / @CantidadTutores ;
+ RETURN
+ END
+GO
+ 
+--drop PROCEDURE spuCalcularAlumnoGrupo
+--DECLARE @NroGrupos INT EXEC spuCalcularAlumnoGrupo @NroGrupos OUTPUT SELECT @NroGrupos
+ 
